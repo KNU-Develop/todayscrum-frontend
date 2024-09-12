017c18edefdc6ea5e7a1ed86570048f0001368bf
seowooda <koeun0409@gmail.com>
2024-09-13 01:42:58 +0900

    fix: 일정(List)에 일정 없을 시 '일정 없음' 출력으로 수정

--- src/components/Calendar/List.tsx
+++ src/components/Calendar/List.tsx
@@ -68,48 +68,54 @@ export function List({ schedules, projects }: ListProps) {
     <div className="flex h-full w-[864px] flex-shrink-0 flex-col items-start gap-[10px] p-4">
       <div className="h-[1px] w-[832px] bg-gray-300" />
       <div className="w-full">
-        {Object.entries(groupedSchedules).map(([key, schedules]) => {
-          const [year, month, date] = key.split('-')
-          const isToday = key === today
-          return (
-            <div
-              key={key}
-              className="flex gap-[10px] self-stretch border-b border-slate-300 p-[10px]"
-            >
-              <div className="flex w-[150px] items-center gap-5 px-4 py-1">
-                <div
-                  className={`${isToday ? 'flex h-9 items-center justify-center rounded-full bg-black text-white' : ''}`}
-                >
-                  <p className="w-9 text-center text-lead">
-                    {formatDate(Number(date))}
-                  </p>
-                </div>
-                <p className="w-opacity-40 py-1 text-detail">{`${month}월 ${date}일`}</p>
-              </div>
-
-              <div className="flex flex-1 flex-col items-start justify-center gap-[10px] py-1">
-                {schedules.map((schedule, index) => (
+        {Object.keys(groupedSchedules).length > 0 ? (
+          Object.entries(groupedSchedules).map(([key, schedules]) => {
+            const [year, month, date] = key.split('-')
+            const isToday = key === today
+            return (
+              <div
+                key={key}
+                className="flex gap-[10px] self-stretch border-b border-slate-300 p-[10px]"
+              >
+                <div className="flex w-[150px] items-center gap-5 px-4 py-1">
                   <div
-                    key={index}
-                    className="flex w-full items-center gap-9 pl-1"
+                    className={`${isToday ? 'flex h-9 items-center justify-center rounded-full bg-black text-white' : ''}`}
                   >
-                    <div className="flex items-center gap-2 self-stretch">
-                      <div
-                        className={`h-2 w-2 rounded-full ${getDotColor(getProjectColor(schedule.projectId ?? ''))}`}
-                      />
-                      <p className="display-webkit-box webkit-box-orient-vertical webkit-line-clamp-2 w-[130px] text-body">
-                        {formatTime(schedule.startDate, schedule.endDate)}
-                      </p>
-                    </div>
-                    <p className="display-webkit-box webkit-box-orient-vertical webkit-line-clamp-1 flex-[1_0_0]">
-                      {schedule.title}
+                    <p className="w-9 text-center text-lead">
+                      {formatDate(Number(date))}
                     </p>
                   </div>
-                ))}
+                  <p className="w-opacity-40 py-1 text-detail">{`${month}월 ${date}일`}</p>
+                </div>
+
+                <div className="flex flex-1 flex-col items-start justify-center gap-[10px] py-1">
+                  {schedules.map((schedule, index) => (
+                    <div
+                      key={index}
+                      className="flex w-full items-center gap-9 pl-1"
+                    >
+                      <div className="flex items-center gap-2 self-stretch">
+                        <div
+                          className={`h-2 w-2 rounded-full ${getDotColor(getProjectColor(schedule.projectId ?? ''))}`}
+                        />
+                        <p className="display-webkit-box webkit-box-orient-vertical webkit-line-clamp-2 w-[130px] text-body">
+                          {formatTime(schedule.startDate, schedule.endDate)}
+                        </p>
+                      </div>
+                      <p className="display-webkit-box webkit-box-orient-vertical webkit-line-clamp-1 flex-[1_0_0]">
+                        {schedule.title}
+                      </p>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })
+        ) : (
+          <div className="flex items-center justify-center py-10">
+            <p className="text-gray-500">일정 없음</p>
+          </div>
+        )}
       </div>
     </div>
   )
