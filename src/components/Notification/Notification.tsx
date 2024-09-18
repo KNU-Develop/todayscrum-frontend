8956e7107e911be03692de0e04cc5466d223e472
gunhee <rjsgml771@naver.com>
2024-09-18 13:53:22 +0900

    fix: 읽은 알림 icon 수정

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -15,6 +15,9 @@ import { useOneProjectInfoQuery } from '@/api/services/project/quries'
 import { useQueryClient } from '@tanstack/react-query'
 import { format } from 'date-fns'
 import { ko } from 'date-fns/locale'
+import { Bell } from 'lucide-react'
+import { BellDot } from 'lucide-react'
+import { ListCheck } from 'lucide-react'
 
 const ProjectInviteModal: React.FC<{
   onClose: () => void
@@ -385,7 +388,9 @@ const NotificationDropdown: React.FC = () => {
 
   const notificationResponse = useNotificationListQuery()
   const notificationList = notificationResponse.data?.result || []
-
+  const readNotNotification = notificationList.some(
+    (notification) => !notification.read,
+  )
   const handleNotificationClick = (notification: Notification) => {
     if (notification.type === NotificationType.ProjectInvite) {
       setNotification(notification)
@@ -420,28 +425,7 @@ const NotificationDropdown: React.FC = () => {
         onClick={toggleDropdown}
         ref={buttonRef}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          fill="none"
-        >
-          <path
-            d="M21 9.33398C21 7.47747 20.2625 5.69699 18.9497 4.38424C17.637 3.07148 15.8565 2.33398 14 2.33398C12.1435 2.33398 10.363 3.07148 9.05025 4.38424C7.7375 5.69699 7 7.47747 7 9.33398C7 17.5007 3.5 19.834 3.5 19.834H24.5C24.5 19.834 21 17.5007 21 9.33398Z"
-            stroke="black"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M16.0181 24.5C15.813 24.8536 15.5186 25.1471 15.1644 25.3511C14.8102 25.5551 14.4086 25.6625 13.9998 25.6625C13.591 25.6625 13.1894 25.5551 12.8352 25.3511C12.481 25.1471 12.1866 24.8536 11.9814 24.5"
-            stroke="black"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        {!readNotNotification ? <Bell size={25} /> : <BellDot size={25} />}
       </button>
 
       {isOpen && (
@@ -451,78 +435,8 @@ const NotificationDropdown: React.FC = () => {
           role="menu"
         >
           <div className="flex items-center justify-between">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                d="M12 5.33398C12 4.27312 11.5786 3.2557 10.8284 2.50556C10.0783 1.75541 9.06087 1.33398 8 1.33398C6.93913 1.33398 5.92172 1.75541 5.17157 2.50556C4.42143 3.2557 4 4.27312 4 5.33398C4 10.0007 2 11.334 2 11.334H14C14 11.334 12 10.0007 12 5.33398Z"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M9.15335 14C9.03614 14.2021 8.86791 14.3698 8.6655 14.4864C8.46309 14.6029 8.2336 14.6643 8.00001 14.6643C7.76643 14.6643 7.53694 14.6029 7.33453 14.4864C7.13212 14.3698 6.96389 14.2021 6.84668 14"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                d="M6.66699 4H14.0003"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M6.66699 8H14.0003"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M6.66699 12H14.0003"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M2 4.00065L2.66667 4.66732L4 3.33398"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M2 8.00065L2.66667 8.66732L4 7.33398"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M2 12.0007L2.66667 12.6673L4 11.334"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <Bell size={20} />
+            <ListCheck size={20} />
           </div>
           <ul className="space-y-2 p-4">
             {notificationList.map((notice) => (
